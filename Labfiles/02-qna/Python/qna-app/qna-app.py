e082816e65db0915d222bc4f8751f2398a1c0de5
simonyuen1 <161483712+simonyuen1@users.noreply.github.com>
2026-01-29 12:20:56 +0000

    Update qna-app.py

--- Labfiles/02-qna/Python/qna-app/qna-app.py
+++ Labfiles/02-qna/Python/qna-app/qna-app.py
@@ -1,9 +1,11 @@
 from dotenv import load_dotenv
 import os
-
+ 
 # Import namespaces
-
-
+# import namespaces
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.language.questionanswering import QuestionAnsweringClient
+ 
 def main():
     try:
         # Get Configuration Settings
@@ -12,17 +14,31 @@ def main():
         ai_key = os.getenv('AI_SERVICE_KEY')
         ai_project_name = os.getenv('QA_PROJECT_NAME')
         ai_deployment_name = os.getenv('QA_DEPLOYMENT_NAME')
-
+ 
         # Create client using endpoint and key
-
-
+        # Create client using endpoint and key
+        credential = AzureKeyCredential(ai_key)
+        ai_client = QuestionAnsweringClient(endpoint=ai_endpoint, credential=credential)
+ 
         # Submit a question and display the answer
-
-
-
+        # Submit a question and display the answer
+        user_question = ''
+        while True:
+            user_question = input('\nQuestion:\n')
+            if user_question.lower() == "quit":                
+                break
+            response = ai_client.get_answers(question=user_question,
+                                            project_name=ai_project_name,
+                                            deployment_name=ai_deployment_name)
+            for candidate in response.answers:
+                print(candidate.answer)
+                print("Confidence: {}".format(candidate.confidence))
+                print("Source: {}".format(candidate.source))
+ 
+ 
     except Exception as ex:
         print(ex)
-
-
+ 
+ 
 if __name__ == "__main__":
     main()
